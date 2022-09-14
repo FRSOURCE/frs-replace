eb9f6c182c47a67f5d1dddf11185b832b7413701
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2022-09-14 16:20:57 +0200

    chore: fix new style standard

--- src/utils.js
+++ src/utils.js
@@ -3,20 +3,20 @@ const writeError = msg => { throw new Error(`@frsource/frs-replace :: ${msg}`) }
 const getReplaceFn = (needle, replacement) =>
   typeof needle === 'string'
     ? content => {
-        const needleLen = needle.length
-        let result = ''
-        let i
-        let endIndex = 0
+      const needleLen = needle.length
+      let result = ''
+      let i
+      let endIndex = 0
 
-        while ((i = content.indexOf(needle, endIndex)) !== -1) {
-          result += content.slice(endIndex, i) + replacement
-          endIndex = i + needleLen
-        }
-
-        result += content.slice(endIndex, content.length)
-
-        return result
+      while ((i = content.indexOf(needle, endIndex)) !== -1) {
+        result += content.slice(endIndex, i) + replacement
+        endIndex = i + needleLen
       }
+
+      result += content.slice(endIndex, content.length)
+
+      return result
+    }
     : content => content.replace(needle, replacement)
 
 module.exports = { writeError, getReplaceFn }
